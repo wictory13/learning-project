9694a3e3223690e739e18d68641bec9348340ba3
wictory13 <vickistar13@mail.ru>
2020-07-05 22:04:33 +0500

    refactor of first try

--- src/index.js
+++ src/index.js
@@ -2,21 +2,46 @@ import * as React from "react";
 import * as ReactDom from "react-dom";
 import css from "./index.css"
 
-class Todo extends React.Component{
-    constructor(props) {
+let todosList = [];
+
+class TodoApp extends React.Component {
+    constructor (props) {
         super(props);
-        this.value = props.value;
+        this.addTodo = this.addTodo.bind(this);
+        this.removeTodo = this.removeTodo.bind(this);
+        this.state = {todos: todosList};
+    }
+
+    addTodo(todo) {
+        todosList.push({
+            value: todo,
+            done: false
+        });
+        this.setState({todos: todosList});
+    }
+
+    removeTodo(todoIndex) {
+        todosList.splice(todoIndex, 1);
+        this.setState({todos: todosList});
     }
 
     render() {
-        return <label>{this.value}</label>;
+        return (
+            <div>
+                <TodoHeader />
+                <TodoForm addTodo={this.addTodo} />
+            </div>
+        );
     }
-
 }
 
-// let allTodos = []
+class TodoHeader extends React.Component {
+    render () {
+        return <h1>todos</h1>;
+    }
+}
 
-class TodoList extends React.Component {
+class TodoForm extends React.Component {
     constructor(props) {
         super(props);
         this.input = React.createRef();
@@ -25,21 +50,18 @@ class TodoList extends React.Component {
 
     keyPressed(event) {
         if (event.key === "Enter") {
-            // allTodos.push(this.input.current.value)
-            return <Todo value={this.input.current.value}/>
+            let newTodo = this.input.current.value;
+            if (newTodo) {
+                this.props.addTodo(newTodo);
+            }
         }
-
     }
 
-    render() {
+    render () {
         return (
-            <div className="todoList">
-                <input onKeyPress={this.keyPressed} ref={this.input} placeholder="What needs to be done?" />
-            </div>
-        )
+            <input onKeyPress={this.keyPressed} ref={this.input} placeholder="What needs to be done?"/>
+        );
     }
 }
 
-
-
-ReactDom.render(<TodoList />, document.getElementById("container"))
+ReactDom.render(<TodoApp />, document.getElementById("container"));
